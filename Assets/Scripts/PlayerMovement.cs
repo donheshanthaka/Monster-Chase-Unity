e4fcaf32a17e52c1cdd63eb29132e21e20b9a0de
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-06 03:23:23 +0530

    Implementation of basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 pos = transform.position;
+
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+
+        transform.position = pos;
     }
+
 }
