78c8e852e58cce05afb1df2f02a224a934435212
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-07 09:50:04 +0530

    Implementation of player walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,19 +32,45 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim.speed = 0.8f;
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMoveKeyboard();
+        AnimatePlayer();
     }
 
+    // Implementation of player movement
     void PlayerMoveKeyboard()
     {
+        // Getting the keyboard input of left and right (A,D,<-, ->)
         movementX = Input.GetAxisRaw("Horizontal");
 
+        // Updating the position using the transform component of the player
         transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
+        // going to the right side
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+
+        } else if (movementX < 0)
+        {
+            // going to the left side
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+
+        } else
+        {
+            // idle
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+        
     }
 }
