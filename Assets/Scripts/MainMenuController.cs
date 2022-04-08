500cafe09f09932e0cf81d1b4fe21ffff1ad6d39
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-08 22:37:36 +0530

    Getting the name of the selected object in main menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,7 +7,10 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
+        // Getting the name of the selected object, only two to be selected...either button 0 or 1 and the converting that name to int
+        int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
         // The scene manager will start playing the scene that correspond to the string passed in
-        SceneManager.LoadScene("Gameplay");
+        //SceneManager.LoadScene("Gameplay");
     }
 } // class
