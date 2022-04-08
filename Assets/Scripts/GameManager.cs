215d8b1a4bcb693fb1c4f343b44ca780d7f488b6
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-09 02:16:10 +0530

    Implementation of the functionality to pass the selected character from the menu to an static variable of GameManager class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // This variable holds an instance for this class, this GameManager class can call its own methods inside the class
+    public static GameManager instance;
+
     [SerializeField]
     private GameObject[] players;
+
+    private int _CharIndex;
+    public int CharIndex
+    {
+        get { return _CharIndex; }
+        set { _CharIndex = value; }
+    }
+
+    private void Awake()
+    {
+        // Sets the instance variable to the current instance
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -10,6 +10,9 @@ public class MainMenuController : MonoBehaviour
         // Getting the name of the selected object, only two to be selected...either button 0 or 1 and the converting that name to int
         int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
 
+        // Setting the selected character index as the character index of the GameManager instance's static variable
+        GameManager.instance.CharIndex = selectedCharacter;
+
         // The scene manager will start playing the scene that correspond to the string passed in
         //SceneManager.LoadScene("Gameplay");
     }
